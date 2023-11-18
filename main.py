d0d3c74528c420e501afa32e30ca4f478319db8b
DashCampbell <dash9campbell@gmail.com>
2023-11-18 16:41:38 -0700

    more developed

--- main.py
+++ main.py
@@ -14,5 +14,24 @@ driver.get("https://www.google.com/search?q=iphone+12&sca_esv=583676307&rlz=1C1R
 elements = driver.find_elements(By.CLASS_NAME, 'sh-dgr__content') 
 for e in elements:
     title = e.find_element(By.CLASS_NAME, "tAxDx")
+    price = e.find_element(By.CLASS_NAME, "a8Pemb")
+    link = e.find_element(By.CLASS_NAME, "Lq5OHe")
     print(title.text)
+    print(price.text)
+    print(link.get_attribute('href'))
+    print()
+
+elements = driver.find_elements(By.CLASS_NAME, 'KZmu8e') 
+for e in elements:
+    brand = e.find_element(By.CLASS_NAME, "E5ocAb")
+    title = e.find_element(By.CLASS_NAME, "sh-np__product-title")
+    price = e.find_element(By.CLASS_NAME, "T14wmb")
+    link = e.find_element(By.CLASS_NAME, "shntl")
+    print(brand.text)
+    print(title.text)
+    print(price.text)
+    print(link.get_attribute('href'))
+    print()
+
+
 driver.quit()
