abad5a972855fba46bdeb0692d879069431c06f2
DashCampbell <dash9campbell@gmail.com>
2023-11-18 14:35:32 -0700

    kind of works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('./chromedriver')
+driver = webdriver.Chrome()
 driver.get("https://www.python.org")
 print(driver.title)
-driver.close()
+driver.quit()
