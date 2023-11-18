dc1a46f63cbc925107ea0aab59d8bd0529eaa245
DashCampbell <dash9campbell@gmail.com>
2023-11-18 16:55:44 -0700

    almost done

--- main.py
+++ main.py
@@ -9,29 +9,36 @@ options.headless = True
 
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.google.com/search?q=iphone+12&sca_esv=583676307&rlz=1C1RXMK_enCA1020CA1020&tbm=shop&source=lnms&sa=X&ved=2ahUKEwjmn4it1c6CAxVOJzQIHYDTD7cQ_AUoAXoECAMQAw&biw=1536&bih=739&dpr=1.25")
+search_query = "apple+watch+ultra+2"
+driver.get(f"https://www.google.com/search?q={search_query}&sca_esv=583676307&rlz=1C1RXMK_enCA1020CA1020&tbm=shop&source=lnms&sa=X&ved=2ahUKEwjmn4it1c6CAxVOJzQIHYDTD7cQ_AUoAXoECAMQAw&biw=1536&bih=739&dpr=1.25")
+
+products = {}
 
 elements = driver.find_elements(By.CLASS_NAME, 'sh-dgr__content') 
 for e in elements:
+    brand = e.find_element(By.CLASS_NAME, "aULzUe").text
     title = e.find_element(By.CLASS_NAME, "tAxDx")
     price = e.find_element(By.CLASS_NAME, "a8Pemb")
     link = e.find_element(By.CLASS_NAME, "Lq5OHe")
-    print(title.text)
-    print(price.text)
-    print(link.get_attribute('href'))
-    print()
+
+    if not brand in products:
+        products[brand] = [{'title':title.text, 'price':price.text,'link':link.get_attribute('href')}]
+    else:
+        products[brand].append({'title':title.text, 'price':price.text,'link':link.get_attribute('href')})
 
 elements = driver.find_elements(By.CLASS_NAME, 'KZmu8e') 
 for e in elements:
-    brand = e.find_element(By.CLASS_NAME, "E5ocAb")
+    brand = e.find_element(By.CLASS_NAME, "E5ocAb").text
     title = e.find_element(By.CLASS_NAME, "sh-np__product-title")
     price = e.find_element(By.CLASS_NAME, "T14wmb")
     link = e.find_element(By.CLASS_NAME, "shntl")
-    print(brand.text)
-    print(title.text)
-    print(price.text)
-    print(link.get_attribute('href'))
-    print()
 
+    if not brand in products:
+        products[brand] = [{'title':title.text, 'price':price.text,'link':link.get_attribute('href')}]
+    else:
+        products[brand].append({'title':title.text, 'price':price.text,'link':link.get_attribute('href')})
+
+for brand in products:
+    print(brand)
 
 driver.quit()
