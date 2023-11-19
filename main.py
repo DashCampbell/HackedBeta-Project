d537899fd08ea067735e03e60b5ea14457416612
Garrick Wong <inquisitor8986@gmail.com>
2023-11-19 01:53:12 -0700

    no change

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from webscraper import webScrape
 
 products = webScrape("apple+watch+ultra+2")
 
-for brand in products:
+'''for brand in products:
     print(brand)
     print(products[brand][0]['title'])
-    print()
+    print()'''
+
+trustedBrands = ['Apple', 'Amazon CA', 'The Source', 'Walmart.ca']
+for brand in products:
+    if brand in trustedBrands:
+        print(brand)
+        print(products[brand][0]['title'])
+        print()
